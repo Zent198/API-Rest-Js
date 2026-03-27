dbc89f8512e5c40a316fdb948741da1be9ca1edc
Zent198 <dvlemoine03@gmail.com>
2026-03-26 20:06:06 -0400

    fix: adding ssl connection for aiven

--- src/config/db.js
+++ src/config/db.js
@@ -10,8 +10,12 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+
 
 pool.getConnection()
     .then(connection => {
