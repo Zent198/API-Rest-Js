6a7df22152a48c196b73ceebd327c70d2bd9ce18
Zent198 <dvlemoine03@gmail.com>
2026-03-26 20:41:16 -0400

    Fix: change the name of the database

--- src/config/confg.js
+++ src/config/confg.js
@@ -6,6 +6,7 @@ export const config = {
         user: process.env.DB_USER,
         pass: process.env.DB_PASS,
         name: process.env.DB_NAME,
+        port: process.env.DB_PORT,
     },
 
     jwt: {
